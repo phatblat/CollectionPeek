21b5fc63cfd720c211eae9eab886fd61ef4e2742
Ben Chatelain <ben@octop.ad>
2015-10-14 21:58:45 -0600

    Change initial scene to be a collection view

---
+++ CollectionPeek/CollectionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewController.swift
+//  CollectionPeek
+//
+//  Created by Ben Chatelain on 10/14/15.
+//  Copyright © 2015 Ben Chatelain. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+
+
+
+}
+
